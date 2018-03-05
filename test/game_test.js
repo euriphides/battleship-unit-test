40c5294bdb0bb752a69e9187a5806b9dde162bde
euriphides <euriphides@gmail.com>
2018-03-05 13:26:25 -0700

    Complete section on mocha --watch functions - Add a return to game_logic/game_instance - Add test for GAME INSTANCE FUNCTIONS in test/game_test - Add mocha file watchers to package.json

--- test/game_test.js
+++ test/game_test.js
@@ -2,7 +2,21 @@ var expect = require('chai').expect;
 
 describe('GAME INSTANCE FUNCTIONS', function(){ // I could also mark this as pending by adding x - "xdescribe" or "xit"
   describe('checkGameStatus', function() {
-    it('should tell me when the game is over'); // by not adding a function, I've created a pending test
-
+    //it('should tell me when the game is over'); // by not adding a function, I've created a pending test
+  var checkGameStatus = require('../game_logic/game_instance.js').checkGameStatus;
+    it('should tell me when the game is over', function() {
+      var players = [
+        {
+          ships: [
+            {
+              locations: [[0,0]],
+              damage: [[0,0]]
+            }
+          ]
+        }
+      ];
+      var actual = checkGameStatus(players);
+      expect(actual).to.be.false;
+    })
   });
 });
